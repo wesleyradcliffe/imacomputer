3df936cdf2d3580c7c62b32091f01f07ac04f188
Wesley D. Radcliffe <wesley.radcliffe@gmail.com>
2021-11-02 20:27:00 -0400

    Initial commit

---
+++ Lesson 1/HelloWorld.js
@@ -0,0 +1,15 @@
+/*
+* ASH AND WYATT. ITS TIME TO FUCKING PROGRAM! LETS DO THIS!
+* This is a 'block comment'. 'Comments' are for PEOPLE ONLY! The Interpreter will ignore it!
+*/
+
+//This is a line comment. Same deal. 
+
+//An individual line is called a 'statement'. A statement usually ends with a semicolon. ;
+
+//You can put the double dashes in front of lines of code to make the computer ignore them.
+//console.log('Hello World');
+
+//Uncomment (remove the dashes) from the line of code above. Then SAVE the file.
+//Once the file is saved, go to your command line and type out
+// `node HelloWorld.js`. NEXT go to variables.js
